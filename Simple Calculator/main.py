19c4cfcb1734cd06d024d5df8b920e37c4b7a1da
Kreyno93 <michael.wagener@neuefische.de>
2026-03-05 16:15:17 +0100

    adding a calculator to the pool; first feature is addition

---
+++ Simple Calculator/main.py
@@ -0,0 +1,7 @@
+print("This is Calculator")
+
+print("Starting off with Addition: ")
+number1 = 5
+number2 = 10
+result = number1 + number2
+print("The result of " + str(number1) + " + " + str(number2) + " is: " + str(result))
