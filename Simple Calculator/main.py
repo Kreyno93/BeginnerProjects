801af602fa7b0adf7c8cdecfbdd00e48956fe02c
Kreyno93 <michael.wagener@neuefische.de>
2026-03-05 16:16:16 +0100

    Adding now the Substraction to the Calc

--- Simple Calculator/main.py
+++ Simple Calculator/main.py
@@ -5,3 +5,9 @@ number1 = 5
 number2 = 10
 result = number1 + number2
 print("The result of " + str(number1) + " + " + str(number2) + " is: " + str(result))
+
+print("Now let's do Subtraction: ")
+number3 = 15
+number4 = 5
+result2 = number3 - number4
+print("The result of " + str(number3) + " - " + str(number4) + " is: " + str(result2))
